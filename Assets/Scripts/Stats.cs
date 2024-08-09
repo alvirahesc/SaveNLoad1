0da65ce841de69af6029128dc57260e15eb7a5d8
KepperBee <alvirahmatullahakbar25@gmail.com>
2024-08-09 14:37:11 +0700

    Setup
    Setting up black cheese

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Source: https://www.youtube.com/watch?v=GoHYSOFiZHc&list=PLujKcy4gXDJXOAvJQgJlCIn3BnZk7wyZp 2:35
+//monobehaviour extension is deleted, this means you cant attach this script to a game object. But you can refer to it inside of a Monobehaviour script for the player
+public class Stats
+{
+    public int atk;
+    public int def;
+    public int piercing;
+    public int hp;
+
+}
